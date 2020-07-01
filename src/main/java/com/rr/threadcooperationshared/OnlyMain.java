ba3b480ee5d5796e7c947e947fa1adacae724dd9
thinkerr24 <thinkerr24@163.com>
2020-07-01 17:11:11 +0800

    02. 线程中止interrupt by implementing Runnable

--- src/main/java/com/rr/threadcooperationshared/OnlyMain.java
+++ src/main/java/com/rr/threadcooperationshared/OnlyMain.java
@@ -4,24 +4,21 @@ package com.rr.threadcooperationshared;
 
 public class OnlyMain {
 
-	private static class UseThread extends Thread {
+	private static class UseThread implements Runnable {
 		
-		public UseThread(String name) {
-			super(name);
-		}
 		@Override
 		public void run() {
 			String threadName = Thread.currentThread().getName() ;
-			while (!isInterrupted()) {
+			while (!Thread.currentThread().isInterrupted()) {
 				System.out.println(threadName + " is running!");
 			}
 			// 进程标志位
-			System.out.println(threadName + " interrput flag is " + isInterrupted());
+			System.out.println(threadName + " interrput flag is " + Thread.currentThread().isInterrupted());
 		}
 	}
 	
 	public static void main(String[] args) throws InterruptedException {
-			Thread endThread = new UseThread("endThread");
+			Thread endThread = new Thread(new UseThread());
 			endThread.start();
 			Thread.sleep(20);
 			endThread.interrupt();
