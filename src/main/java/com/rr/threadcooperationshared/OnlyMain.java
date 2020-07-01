a8ad50079c72ccbd6a8e9fac45ac49953097f14b
thinkerr24 <thinkerr24@163.com>
2020-07-01 17:41:53 +0800

    03. 线程中抛出InterruptedException要在catch中调interrupt()

--- src/main/java/com/rr/threadcooperationshared/OnlyMain.java
+++ src/main/java/com/rr/threadcooperationshared/OnlyMain.java
@@ -1,27 +1,38 @@
 package com.rr.threadcooperationshared;
 
-
-
 public class OnlyMain {
 
-	private static class UseThread implements Runnable {
-		
+	private static class UseThread extends Thread {
+
+		public UseThread(String name) {
+			super(name);
+		}
+
 		@Override
 		public void run() {
-			String threadName = Thread.currentThread().getName() ;
-			while (!Thread.currentThread().isInterrupted()) {
-				System.out.println(threadName + " is running!");
+			String threadName = getName();
+			while (!isInterrupted()) {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					System.out.println(threadName + " interrupt flag is " + isInterrupted());
+					// 方法中发生异常, 线程的中断标志位会被复位成false，如果确实是需要中断线程，要求我们自己在catch语句块里再次调用interrupt()
+					interrupt();
+					e.printStackTrace();
+				}
+
+				System.out.println(threadName);
 			}
-			// 进程标志位
-			System.out.println(threadName + " interrput flag is " + Thread.currentThread().isInterrupted());
+
+			System.out.println(threadName + " interrupt flag is " + isInterrupted());
 		}
 	}
-	
+
 	public static void main(String[] args) throws InterruptedException {
-			Thread endThread = new Thread(new UseThread());
-			endThread.start();
-			Thread.sleep(20);
-			endThread.interrupt();
+		Thread endThread = new UseThread("HasInterrputException");
+		endThread.start();
+		Thread.sleep(20);
+		endThread.interrupt();
 	}
-	
+
 }
