8b6ff8f8486c0094d6947c72ab61967edf319a26
thinkerr24 <thinkerr24@163.com>
2020-07-01 13:55:35 +0800

    02. 线程中止interrupt by extending ThreadClass

--- src/main/java/com/rr/threadcooperationshared/OnlyMain.java
+++ src/main/java/com/rr/threadcooperationshared/OnlyMain.java
@@ -1,30 +1,30 @@
 package com.rr.threadcooperationshared;
 
-import java.lang.management.ManagementFactory;
-import java.lang.management.ThreadInfo;
-import java.lang.management.ThreadMXBean;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
+
 
 public class OnlyMain {
 
-	// 实现Callable接口，允许run方法有返回值
-	private static class UseCall implements Callable<String> {
-
-		@Override
-		public String call() throws Exception {
-			System.out.println("I'm in call()");
-			return "CallResult";
-		}
+	private static class UseThread extends Thread {
 		
+		public UseThread(String name) {
+			super(name);
+		}
+		@Override
+		public void run() {
+			String threadName = Thread.currentThread().getName() ;
+			while (!isInterrupted()) {
+				System.out.println(threadName + " is running!");
+			}
+			// 进程标志位
+			System.out.println(threadName + " interrput flag is " + isInterrupted());
+		}
 	}
 	
-	public static void main(String[] args) throws InterruptedException, ExecutionException {
-			FutureTask<String> futureTask = new FutureTask<>(new UseCall());
-			// FutureTask实现了Runnable接口
-			new Thread(futureTask).start();
-			System.out.println(futureTask.get());
+	public static void main(String[] args) throws InterruptedException {
+			Thread endThread = new UseThread("endThread");
+			endThread.start();
+			Thread.sleep(20);
+			endThread.interrupt();
 	}
 	
 }
